faa323fb3183c3ddb0db4f9b51c65f57437867ec
huzaifam0 <hmemon01@uoguelph.ca>
2026-09-18 12:57:58 -0400

    Updated name of project

--- database.c
+++ database.c
@@ -1,22 +1,8 @@
 /*
-Student Name: Huzaifa Memon
-Student ID: 1373314
-Course: CIS*1300 | Assignment 5
-Due Date: November 21st, at 11:59pm
-
-By including this statement in this header comment, I certify that I have read and understood the University policy on academic integrity.
-
-I assert that this work is 100% my own. 
-I assert that I did not copy code from another person.
-I assert that I did not copy code from AI.
-I understand that I will receive a grade of 0 if I copy code from another person or from AI, as this constitutes academic dishonesty.
-
-I have appropriately acknowledged all materials (code, data, images, ideas or words) that I have used, whether directly quoted or paraphrased.
-
-Furthermore, I certify that this assignment was prepared by me specifically for the CIS*1300 Fall 2025 course.
+Name: Huzaifa Memon
 */      
 
-#include "a5.h"
+#include "database.h"
 
 #define MAX_DNA_LENGTH 5096
 
